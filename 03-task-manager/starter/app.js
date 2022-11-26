969c3629f7f5083a5a1dfe6c3cc14e05af220104
shashank-jain2605 <shashank.jain2605@gmail.com>
2022-11-27 04:28:08 +0530

    route chages

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,2 +1,20 @@
-console.log("Task Manager App");
-console.log("hi");
+const express = require("express");
+const tasks = require("./routes/task");
+const app = express();
+
+const port = 3001;
+
+//middleware
+app.use(express.json());
+
+// setting up basic route
+app.use("/api/v1/tasks", tasks);
+
+//routes
+app.get("/hello", (req, res) => {
+  res.status(200).send("hello server is running");
+});
+
+app.listen(port, () => {
+  console.log(`server running on ${port}`);
+});
